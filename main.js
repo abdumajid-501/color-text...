052b280dd33d66644e9f9970bfbac3b0b191417c
abdumajid-501 <dasturchi501@gmail.com>
2025-10-28 03:27:31 +0500

    men reset tugmasini qoshdim va ishlashini togirladim

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const bodyEl = document.body
 const formEl = document.querySelector(".form")
 const wrappwerEl = document.querySelector(".wrapper")
+const btnEl = document.querySelector(".btn-reset")
 
-const colors = []
+const colors = JSON.parse(localStorage.getItem("colors")) || [];
+
+
+const createSquareEl = (colors) => {
+    wrappwerEl.innerHTML = ""
+    colors.forEach((color) => {
+        let squareEl = document.createElement("span");
+        squareEl.classList.add("square")
+        squareEl.style.backgroundColor = color;
+        wrappwerEl.append(squareEl); 
+    });
+}
+
+createSquareEl(colors)
 
 formEl.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -11,11 +25,9 @@ formEl.addEventListener("submit", (e) => {
     
     bodyEl.style.backgroundColor = color;
     if (bodyEl.style.backgroundColor === color && colors[colors.length - 1] != color) {
-        let squareEl = document.createElement("span");
-        squareEl.classList.add("square")
-        squareEl.style.backgroundColor = color;
-        wrappwerEl.append(squareEl); 
         colors.push(color)
+        createSquareEl(colors)
+        localStorage.setItem("colors", JSON.stringify(colors));
     }
 
 
@@ -25,4 +37,12 @@ formEl.addEventListener("submit", (e) => {
 
 
     formEl.reset();
+})
+
+
+
+btnEl.addEventListener("click", () => {
+    localStorage.clear();
+
+    location.reload();
 })
